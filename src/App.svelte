36f712d9eb3362e2accdbf0e65b2105b0d1ccaef
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-12-11 21:32:01 +0300

    Update
    did the header part(just touched on it)

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,34 @@
+<script>
+
+</script>
+
 <main>
-    
+    <!-- the main container for the website -->
+  <div id="main" class="">
+    <!-- the header -->
+    <header class="mx-10">
+      <!-- Navbar -->
+      <nav class="flex justify-between items-center p-5">
+        <div>
+          <img src="./images/logo.svg" alt="">
+        </div>
+        <div >
+          <button >Try It Free</button>
+        </div>
+      </nav>
+      <!-- Hero section -->
+       <div class="flex justify-around items-center">
+       <!-- hero text -->
+        <div class="flex-1">
+          <h1 class="text-3xl font-bold my-3">Build The Community <br /> Your Fans Will Love</h1>
+          <p class="my-3 mb-7"> Huddle re-imagines the way we build communities. You have <br /> a voice, but so does your audience. 
+            Create connections with <br /> your users as you engage in genuine discussion.</p>
+          <!-- Button -->
+        <a href="/" class="bg-pink-500 text-white p-4 my-10 rounded-full"> Get Started For Free</a>
+        </div>
+        <!-- hero image -->
+         <img width="20px" class="flex-1"src="./images/illustration-mockups.svg" alt="">
+       </div>
+    </header>
+  </div>
 </main>
