c7a95a39546b98aba6d3c55aacc39f558209015b
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-12-12 19:01:39 +0300

    Update App.svelte
    added more things!
    like finished the header, and added the things next to the header

--- src/App.svelte
+++ src/App.svelte
@@ -2,33 +2,81 @@
 
 </script>
 
-<main>
-    <!-- the main container for the website -->
+<main class="" >
+    <!-- ANCHOR the main container for the website -->
   <div id="main" class="">
     <!-- the header -->
-    <header class="mx-10">
-      <!-- Navbar -->
-      <nav class="flex justify-between items-center p-5">
+    <header class="px-12 pb-10 bg-sky-50" style="background-image: url('../images/bg-hero-desktop.svg');">
+      <!-- ANCHOR Navbar -->
+      <nav class="flex justify-between items-center p-5 py-10">
         <div>
           <img src="./images/logo.svg" alt="">
         </div>
         <div >
-          <button >Try It Free</button>
+          <button class="p-3 bg-white rounded-full px-7 shadow-md font-bold">Try It Free</button>
         </div>
       </nav>
-      <!-- Hero section -->
-       <div class="flex justify-around items-center">
-       <!-- hero text -->
-        <div class="flex-1">
-          <h1 class="text-3xl font-bold my-3">Build The Community <br /> Your Fans Will Love</h1>
-          <p class="my-3 mb-7"> Huddle re-imagines the way we build communities. You have <br /> a voice, but so does your audience. 
+      <!-- ANCHOR Hero section -->
+       <div class="flex justify-around items-center p-5">
+       <!-- ANCHOR hero text -->
+        <div class="flex-1 mb-20">
+          <h1 class="text-4xl tracking-wide font-bold my-3 text-slate-800">Build The Community <br /> Your Fans Will Love</h1>
+          <p class="my-3 mb-12 tracking-wide mt-6"> Huddle re-imagines the way we build communities. You have <br /> a voice, but so does your audience. 
             Create connections with <br /> your users as you engage in genuine discussion.</p>
           <!-- Button -->
-        <a href="/" class="bg-pink-500 text-white p-4 my-10 rounded-full"> Get Started For Free</a>
+        <a href="/" class= " bg-pink-500 text-white p-4 my-10 px-16 rounded-full"> Get Started For Free</a>
         </div>
-        <!-- hero image -->
+        <!-- ANCHOR hero image -->
          <img width="20px" class="flex-1"src="./images/illustration-mockups.svg" alt="">
        </div>
     </header>
+
+
+
+
+
+
+    <!-- ANCHOR descriptions! -->
+     <section>
+        <!-- *First div -->
+        <div class=" flex justify-between items-center m-32 my-10 px-10 mt-10 border p-10">
+          <!-- text -->
+           <div class="flex-1">
+            <h1 class="text-3xl font-bold mb-5 text-slate-800">Grow Together</h1>
+            <p class="mt-5 text-slate-700">Generate meaningful discussions with your audience and <br /> build a strong, loyal community. 
+              Think of the insightful <br /> conversations you miss out on with a feedback form. </p>
+           </div>
+           <!-- image -->
+            <img class="flex-1 m-20 my-5 mr-0" width="10px" src="../images/illustration-grow-together.svg" alt="">
+        </div>
+
+        <!-- *Second div -->
+        <div class=" flex justify-between items-center m-32 px-10 mt-10 my-10 border p-10">
+
+          <!-- image -->
+          <img class="flex-1 m-20 my-5 ml-0" width="10px" src="../images/illustration-flowing-conversation.svg" alt="">
+          <!-- text -->
+           <div class="flex-1 ml-10">
+            <h1 class="text-3xl font-bold mb-5 text-slate-800">Grow Together</h1>
+            <p class="mt-5 text-slate-700">Generate meaningful discussions with your audience and <br /> build a strong, loyal community. 
+              Think of the insightful <br /> conversations you miss out on with a feedback form. </p>
+           </div>
+           
+        </div>
+
+
+        <!-- *Third div -->
+        <div class=" flex justify-between items-center m-32 my-10 px-10 mt-10 border p-10">
+          <!-- text -->
+           <div class="flex-1">
+            <h1 class="text-3xl font-bold mb-5 text-slate-800">Your Users</h1>
+            <p class="mt-5 text-slate-700">It takes no time at all to integrate Huddle with your app's <br /> authentication solution. 
+              This means, once signed in to <br />your app, your users can start chatting immediately.</p>
+           </div>
+           <!-- image -->
+            <img class="flex-1 m-20 my-5 mr-0" width="10px" src="../images/illustration-your-users.svg" alt="">
+        </div>
+     </section>
+
   </div>
 </main>
