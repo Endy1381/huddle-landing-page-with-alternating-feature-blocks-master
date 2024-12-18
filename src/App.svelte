1f5166fe50e2e4d10d037b240749c57a4e95c2be
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-12-18 21:09:35 +0300

    Update app.svelte
    almost done with the responsive part

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
     <!-- ANCHOR the main container for the website -->
   <div id="main" class="">
     <!-- the header -->
-    <header class="px-12 pb-10 bg-sky-50" style="background-image: url('../images/bg-hero-desktop.svg');">
+    <header class="lg:px-12 pb-10 bg-sky-50" style="background-image: url('../images/bg-hero-desktop.svg');">
       <!-- ANCHOR Navbar -->
       <nav class="flex justify-between items-center p-5 py-10">
         <div>
@@ -17,7 +17,7 @@
         </div>
       </nav>
       <!-- ANCHOR Hero section -->
-       <div class="flex justify-around items-center p-5">
+       <div class="flex lg:flex-row flex-col text-center lg:text-start justify-around items-center p-5">
        <!-- ANCHOR hero text -->
         <div class="flex-1 mb-20">
           <h1 class="text-4xl tracking-wide font-bold my-3 text-slate-800">Build The Community <br /> Your Fans Will Love</h1>
@@ -27,7 +27,7 @@
         <a href="/" class= " bg-pink-500 text-white p-4 my-10 px-16 rounded-full"> Get Started For Free</a>
         </div>
         <!-- ANCHOR hero image -->
-         <img width="20px" class="flex-1"src="./images/illustration-mockups.svg" alt="">
+         <img  class="flex-1 w-96" src="./images/illustration-mockups.svg" alt="">
        </div>
     </header>
 
@@ -39,51 +39,51 @@
     <!-- ANCHOR descriptions! -->
      <section>
         <!-- *First div -->
-        <div class=" flex justify-between items-center m-32 my-10 px-10 mt-10 border p-10">
+        <div class=" flex lg:flex-row flex-col-reverse justify-between items-center m-10 lg:m-32 my-10 px-10 mt-10 border lg:p-10 p-">
           <!-- text -->
-           <div class="flex-1">
+           <div class="flex-1 text-center lg:text-start">
             <h1 class="text-3xl font-bold mb-5 text-slate-800">Grow Together</h1>
             <p class="mt-5 text-slate-700">Generate meaningful discussions with your audience and <br /> build a strong, loyal community. 
               Think of the insightful <br /> conversations you miss out on with a feedback form. </p>
            </div>
            <!-- image -->
-            <img class="flex-1 m-20 my-5 mr-0" width="10px" src="../images/illustration-grow-together.svg" alt="">
+            <img class="flex-1 m-20 my-5 lg:mr-0 w-96"  src="../images/illustration-grow-together.svg" alt="">
         </div>
 
         <!-- *Second div -->
-        <div class=" flex justify-between items-center m-32 px-10 mt-10 my-10 border p-10">
+        <div class=" flex lg:flex-row flex-col justify-between items-center lg:m-32 m-10 px-10 mt-10 my-10 border  lg:p-10 p-3">
 
           <!-- image -->
-          <img class="flex-1 m-20 my-5 ml-0" width="10px" src="../images/illustration-flowing-conversation.svg" alt="">
+          <img class="flex-1 m-20 my-5 lg:ml-0 w-96"  src="../images/illustration-flowing-conversation.svg" alt="">
           <!-- text -->
-           <div class="flex-1 ml-10">
+           <div class="flex-1 lg:ml-10 lg:text-start text-center lg:text-end">
             <h1 class="text-3xl font-bold mb-5 text-slate-800">Flowing Conversations</h1>
             <p class="mt-5 text-slate-700"> You wouldn't paginate a conversation in real life, so why do <br /> it online? Our threads 
-              have just-in-time loading for a more <br /> natural flow. </p>
+              have just-in-time  loading for a more <br /> natural flow. </p>
            </div>
            
         </div>
 
 
         <!-- *Third div -->
-        <div class=" flex justify-between items-center m-32 my-10 px-10 mt-10 border p-10">
+        <div class=" flex justify-between lg:flex-row flex-col-reverse items-center lg:m-32 m-10 my-10 px-10 mt-10 border p-10">
           <!-- text -->
-           <div class="flex-1">
+           <div class="flex- text-center lg:text-start">
             <h1 class="text-3xl font-bold mb-5 text-slate-800">Your Users</h1>
             <p class="mt-5 text-slate-700">It takes no time at all to integrate Huddle with your app's <br /> authentication solution. 
               This means, once signed in to <br />your app, your users can start chatting immediately.</p>
            </div>
            <!-- image -->
-            <img class="flex-1 m-20 my-5 mr-0" width="10px" src="../images/illustration-your-users.svg" alt="">
+            <img class="flex-1 m-20 my-5 lg:mr-0 w-96"  src="../images/illustration-your-users.svg" alt="">
         </div>
      </section>
 
      <!-- ANCHOR floating call to action -->
-      <section class="flex flex-col justify-center ">
-        <div class="text-center border rounded-md py-10 inline-block p-50 mx-96 ">
+      <section class="flex flex-col justify-center items-center p-5">
+        <div class="text-center border rounded-md py-10 inline-block p-50 lg:mx-96 ">
           <div class="">
             <h1 class="mb-10 text-3xl tracking-wide font-bold text-slate-800 p-2 ">Ready To Build Your Community?</h1>
-            <a href="/" class= " bg-pink-500 text-white p-4  px-16 mb-50 rounded-full"> Get Started For Free</a>
+            <a href="/" class= " bg-pink-500 text-white p-4  px-2 lg:px-16 mb-50 rounded-full"> Get Started For Free</a>
           </div>
         </div>
       </section>
