c7034c03048305cd1b98a973a9fcf8057a2bbf9d
Endy1381 <119288346+Endy1381@users.noreply.github.com>
2024-12-19 21:19:06 +0300

    Update app.svelte
    finished it

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,7 @@
           <img src="./images/logo.svg" alt="">
         </div>
         <div >
-          <button class="p-3 bg-white rounded-full px-7 shadow-md font-bold">Try It Free</button>
+          <button class="p-3 bg-white hover:text-gray-600 rounded-full px-7 shadow-md font-bold">Try It Free</button>
         </div>
       </nav>
       <!-- ANCHOR Hero section -->
@@ -24,7 +24,7 @@
           <p class="my-3 mb-12 tracking-wide mt-6"> Huddle re-imagines the way we build communities. You have <br /> a voice, but so does your audience. 
             Create connections with <br /> your users as you engage in genuine discussion.</p>
           <!-- Button -->
-        <a href="/" class= " bg-pink-500 text-white p-4 my-10 px-16 rounded-full"> Get Started For Free</a>
+        <a href="/" class= " bg-pink-500 hover:bg-pink-400 text-white p-4 my-10 px-16 rounded-full"> Get Started For Free</a>
         </div>
         <!-- ANCHOR hero image -->
          <img  class="flex-1 w-96" src="./images/illustration-mockups.svg" alt="">
@@ -83,16 +83,16 @@
         <div class="text-center border rounded-md py-10 inline-block p-50 lg:mx-96 ">
           <div class="">
             <h1 class="mb-10 text-3xl tracking-wide font-bold text-slate-800 p-2 ">Ready To Build Your Community?</h1>
-            <a href="/" class= " bg-pink-500 text-white p-4  px-2 lg:px-16 mb-50 rounded-full"> Get Started For Free</a>
+            <a href="/" class= " bg-pink-500 hover:bg-pink-400 text-white p-4  px-2 lg:px-16 mb-50 rounded-full"> Get Started For Free</a>
           </div>
         </div>
       </section>
 
 
       <!-- ANCHOR footer -->
-      <footer class=" bg-teal-950 flex justify-between items-center p-16  pt-24">
+      <footer class=" bg-teal-950 flex lg:flex-row flex-col justify-between lg:items-center p-16  pt-24">
         <!-- div1 -->
-         <div>
+         <div class="lg:mb-auto mb-10">
             <img src="../images/logo-c.svg" class="text-white" alt="">
             <ul class="text-white bruh">
               <div class="flex  items-center"> <img src="../images/icon-location.svg" alt="" class=" w-5 h-5 mb-10 mr-5">  <li class="my-5">Lorem ipsum dolor sit amet, consectetur <br />  adipisicing elit. Commodi blanditiis dolorem veniam <br /> explicabo placeat obcaecati exercitationem.</li></div>
@@ -102,8 +102,8 @@
          </div>
 
          <!-- div2 -->
-          <div class="flex text-white">
-            <div class="mr-20">
+          <div class="flex lg:flex-row flex-col text-2xl lg:text-auto lg:mb-0 mb-10 text-white">
+            <div class="lg:mr-20">
               <p>About us</p>
               <p>What we do</p>
               <p>FAQ</p>
@@ -116,12 +116,21 @@
           </div>
           <!-- div3 -->
 
-           <div class="flex text-white">
-            <p>facebook</p>
-            <p>tweeter</p>
-            <p>insta</p>
+           <div class="flex text-white justify-center lg:justify-start">
+               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+               </svg>
+               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
+                   <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+               </svg>
+               <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+                   <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+               </svg>
+
            </div>
+
       </footer>
+      <p class="flex p-3 bg-teal-950 text-white lg:justify-end justify-center">copywright 2022 Huddle(made by Natnael yohannes(Endy)</p>
      
         
      </div>
